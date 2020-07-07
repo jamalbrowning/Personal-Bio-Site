a978fc805f98c3c1379fafd8ab9ca81337d023a8
Jim Browning <jamalebrowning@gmail.com>
2020-07-07 18:41:04 -0500

    added css

--- src/javascripts/components/buildProjects/buildProjects.js
+++ src/javascripts/components/buildProjects/buildProjects.js
@@ -1,5 +1,6 @@
 import projectData from '../../helpers/data/projectData';
 import utils from '../../helpers/utils';
+import './buildProjecs.scss';
 
 const projectBuilder = () => {
   projectData.getProjects()
@@ -7,12 +8,12 @@ const projectBuilder = () => {
       console.warn('get projects worked', projects);
       let domString = `
         <h1>Projects</h1>
-        <div class="d-flex flex-wrap justify-content-between">
+        <div class="proCards d-flex flex-wrap justify-content-center">
       `;
 
       projects.forEach((project) => {
         if (project.available === true) {
-          domString += `<div class="card" style="width: 18rem;">
+          domString += `<div class="card text-center" style="width: 18rem;">
           <div class="card-header">
             <h2 class="title text-dark">${project.title}</h2>
           </div>
@@ -20,7 +21,7 @@ const projectBuilder = () => {
             <li class="list-group-item">${project.description}</li>
             <li class="list-group-item">${project.technologiesUsed}</li>
             <li class="list-group-item">Vestibulum at eros</li>
-            <button class="btn btn-dark"><a href="${project.githubUrl}" class="card-link" target="_blank">Github</a></button>
+            <li class="anchor-projects"><a href="${project.githubUrl}" class="card-link" target="_blank"><i class="fab fa-github-square fa-3x"></i></a></button></li>
           </ul>
         </div>`;
         }
