3ef8fcfb232d4f1810615145ceadf1c918b19e62
Jim Browning <jamalebrowning@gmail.com>
2020-07-07 16:36:33 -0500

    printed projects to div

--- src/javascripts/components/buildProjects/buildProjects.js
+++ src/javascripts/components/buildProjects/buildProjects.js
@@ -1,12 +1,32 @@
 import projectData from '../../helpers/data/projectData';
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
 
 const projectBuilder = () => {
   projectData.getProjects()
-    .then((projects) => console.warn('get projects worked', projects))
+    .then((projects) => {
+      console.warn('get projects worked', projects);
+      let domString = `
+        <h1>Projects</h1>
+        <div class="d-flex flex-wrap">
+      `;
+
+      projects.forEach((project) => {
+        if (project.available === true) {
+          domString += `<div class="card" style="width: 18rem;">
+          <div class="card-header">
+            TITLE GOES HERE
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">${project.description}</li>
+            <li class="list-group-item">${project.technologiesUsed}</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </div>`;
+        }
+      });
+      utils.printToDom('#projectCards', domString);
+    })
     .catch((err) => console.error('get pros broke', err));
-  // const domString = '<h1>I think its working</h1>';
-  // utils.printToDom('#projectCards', domString);
 };
 
 export default { projectBuilder };
