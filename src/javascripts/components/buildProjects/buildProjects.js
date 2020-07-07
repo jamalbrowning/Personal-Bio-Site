67e5becd54e2a5a1c14f6c9cb2138af37142c66c
Jim Browning <jamalebrowning@gmail.com>
2020-07-07 18:04:12 -0500

    populate projects to dom vis firebase data

--- src/javascripts/components/buildProjects/buildProjects.js
+++ src/javascripts/components/buildProjects/buildProjects.js
@@ -7,19 +7,20 @@ const projectBuilder = () => {
       console.warn('get projects worked', projects);
       let domString = `
         <h1>Projects</h1>
-        <div class="d-flex flex-wrap">
+        <div class="d-flex flex-wrap justify-content-between">
       `;
 
       projects.forEach((project) => {
         if (project.available === true) {
           domString += `<div class="card" style="width: 18rem;">
           <div class="card-header">
-            TITLE GOES HERE
+            <h2 class="title text-dark">${project.title}</h2>
           </div>
           <ul class="list-group list-group-flush">
             <li class="list-group-item">${project.description}</li>
             <li class="list-group-item">${project.technologiesUsed}</li>
             <li class="list-group-item">Vestibulum at eros</li>
+            <button class="btn btn-dark"><a href="${project.githubUrl}" class="card-link" target="_blank">Github</a></button>
           </ul>
         </div>`;
         }
